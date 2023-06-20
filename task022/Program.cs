67dcc2da3474c7fa148a6eb42987e0fd0007b510
Sofia <rybina.sofia@gmail.com>
2023-06-20 19:01:42 +0300

    Таблица квадратов task022

---
+++ task022/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine()!;
+    int result = Math.Abs(Convert.ToInt32(value));
+    return result;
+}
+
+void GetNum(int num)
+{
+    int i = 1;
+    while(i <= num)
+    {
+        if(i == 1)
+        {
+            Console.Write(((i * i)));
+            i++;
+        }
+        else
+        {
+            Console.Write(","+ " "+ (i * i));
+            i++;
+        }
+    }
+}
+
+int n = Prompt("Input N: ");
+GetNum(n);
